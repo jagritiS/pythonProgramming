7a79349c75eb2517b366c92cac4e148e5b4365e9
Jagriti Srivastava <jagritisrvstv@gmail.com>
2020-07-27 16:21:17 +0545

    Hello World in Python
    Hello World in python in multiple ways

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+
+#saying helloworld with python
+
+#print Hello World Directly 
+print("Hello World")
+
+#print Hello World from a string
+stringOne = "Hello World"
+print(stringOne)
+
+#print Hello World using format
+print('{0}'.format(stringOne))
